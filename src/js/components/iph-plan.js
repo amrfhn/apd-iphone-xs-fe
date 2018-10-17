f0f0260dd258d76fa52e0a33049f6c51ce7ea8fc
Amir Jefri <amirfarhan.jfri@gmail.com>
2018-10-17 15:59:31 +0800

    update

--- src/js/components/iph-plan.js
+++ src/js/components/iph-plan.js
@@ -6,13 +6,26 @@
 
       var tab = $(this).data("priceplan");
 
-      $(".plan-tab").hide();
-      $("."+tab).toggle();
+      //$(".plan-tab").hide();
+
+      $(".plan-tab").removeClass("isActive");
+      $("."+tab).addClass("isActive");
+
       $(".priceplan-btn").addClass("plan-active");
       $(".priceplan-btn").not(this).removeClass("plan-active");
-      $(".plan-tab").addClass("isActive");
-      $(".plan-tab").not(this).removeClass("isActive");
+      //$(".plan-tab").addClass("isActive");
+      //$(".plan-tab").not(this).removeClass("isActive");
 
     });
+
+    var pricetab = $(".price-tab");
+
+    if (pricetab.length>0){
+      var plantab = pricetab.find(".plan-tab")
+      plantab.each(function(){
+        var self = $(this)
+
+      })
+    }
     
 });
